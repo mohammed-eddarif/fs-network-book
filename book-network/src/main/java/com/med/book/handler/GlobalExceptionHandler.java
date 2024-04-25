b0dc134270fc48022083ce1065dee604ae179b51
mohammed-eddarif <mohammed.eddarif06@gmail.com>
2024-04-25 01:00:47 +0100

    start exceptions handling implementation

---
+++ book-network/src/main/java/com/med/book/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.med.book.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.med.book.handler.BusinessErrorCodes.ACCOUNT_LOCKED;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exp) {
+        return ResponseEntity
+                .status(UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(ACCOUNT_LOCKED.getCode())
+                                .businessErrorDescription(ACCOUNT_LOCKED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+}
